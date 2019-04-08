c3b46215d625af20a4cefab6b8e5fbd598d3c711
Metin İrden <metin.irden@ritmusconsulting.com>
2019-04-08 17:08:06 +0300

    Login&Create pages

---
+++ Chatty/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Vektora.Authentication.Interfaces;
+
+namespace Chatty.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IMembershipProvider<IdentityUser, IdentityRole, KaleUstaPortalContext> _membershipProvider;
+
+        public AccountController(IMembershipProvider<IdentityUser, IdentityRole, KaleUstaPortalContext> membershipProvider) => _membershipProvider = membershipProvider;
+
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var signInResult = await _membershipProvider.SignIn(model.Email, model.Password, true, false);
+            if (signInResult.Succeeded) return RedirectToAction("Index", "Home");
+            return Json(signInResult);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(LoginViewModel model)
+        {
+            IdentityResult result = await _membershipProvider.CreateUser(new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email
+            }, model.Password);
+            TempData["SuccessMail"] = model.Email;
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Logout()
+        {
+            _membershipProvider.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [EmailAddress, Required]
+        public string Email { get; set; }
+
+        [MinLength(8), Required]
+        public string Password { get; set; }
+    }
+}
--- Chatty/Controllers/HomeController.cs
+++ Chatty/Controllers/HomeController.cs
@@ -4,48 +4,20 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Vektora.Authentication.Interfaces;
 
 namespace Chatty.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IMembershipProvider<IdentityUser, IdentityRole, KaleUstaPortalContext> _membershipProvider;
         private readonly IConversationRepository _conversationRepository;
 
-        public HomeController(IMembershipProvider<IdentityUser, IdentityRole, KaleUstaPortalContext> membershipProvider, IConversationRepository conversationRepository)
-        {
-            _membershipProvider = membershipProvider;
-            _conversationRepository = conversationRepository;
-        }
-
-        [Route("~/auth/sign-in")]
-        public async Task<IActionResult> Login(string email, string password)
-        {
-            var signInResult = await _membershipProvider.SignIn(email, password, true, false);
-            if (signInResult.Succeeded) return RedirectToAction("Index", "Home");
-            return Json(signInResult);
-        }
-
-        [Route("~/auth/create")]
-        public async Task<IActionResult> CreateUser(string email, string password)
-        {
-            IdentityResult result = await _membershipProvider.CreateUser(new IdentityUser
-            {
-                Email = email,
-                UserName = email
-            }, password);
-            return Json(result);
-        }
+        public HomeController(IConversationRepository conversationRepository) => _conversationRepository = conversationRepository;
 
         [Authorize]
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
+        [Authorize]
         public async Task<IActionResult> History(string otherMember)
         {
             string convId = await _conversationRepository.GetOrCreateConversationId(new[] { otherMember, User.FindFirstValue(ClaimTypes.NameIdentifier) });
